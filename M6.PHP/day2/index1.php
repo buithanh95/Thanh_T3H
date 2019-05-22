985698847c178b4c7916ba68f7b6b80b0912a8ce
buithanh95 <buithanhvnhy@gmail.com>
2019-05-22 20:44:38 +0700

    ok

---
+++ M6.PHP/day2/index1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>câu lệnh điều kiện trong php</h1>
+<?php
+$age = 19;
+if($age >18){
+    echo "<br> người trưởng thành";
+}else {
+    echo "<br> trẻ em";
+}
+?>
+</body>
+</html>
